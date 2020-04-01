a4e2cc5a6c137a6ac112b7d274aa77442c838e17
anayarojo <raulmartin_04@hotmail.com>
2020-03-31 20:34:29 -0700

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel<br/><strong>To Do List</strong>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/anayarojo/laravel-todo-list">Get started</a>
+                    <a href="https://github.com/anayarojo/laravel-todo-list">Documentation</a>
+                    <a href="https://github.com/anayarojo/laravel-todo-listl">GitHub</a>
                 </div>
             </div>
         </div>
