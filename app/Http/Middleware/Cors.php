b856f461c0a0a20670bf75bf330431eb848bdf53
anayarojo <raulmartin_04@hotmail.com>
2020-04-06 13:01:41 -0700

    Update cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Max-Age: 1000')
+            ->header('Access-Control-Max-Age', '1000')
             ->header('Access-Control-Allow-Origin', '*')
-            ->header("Access-Control-Allow-Credentials: true")
+            ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');
     }
