91586159c8565b1d54497384fe0ccfadf9b4ac34
anayarojo <raulmartin_04@hotmail.com>
2020-04-06 12:52:17 -0700

    Update cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,9 +37,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'cors',
             'throttle:60,1',
             'bindings',
+            'cors',
         ],
     ];
 
